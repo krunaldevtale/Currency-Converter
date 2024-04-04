2b2a2b22d11be5c8a2fc6e742d100d5a4ec981a0
krunaldevtale <krunaldevtale79@gmail.com>
2024-04-04 14:16:17 +0530

    api updated

--- src/Hooks/useCurr.js
+++ src/Hooks/useCurr.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 function useCurr(currency) {
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
             .then((res) => res.json()).then((res) => setData(res[currency]))
     }), [currency]
     return data;
